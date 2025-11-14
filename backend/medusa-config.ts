423a99d2cf20e4018e8f984346dbad59760d1d04
Yash-Sheliya-Apexture <yash@apexture.in>
2025-11-14 15:05:22 +0530

    Fix Medusa config module format

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -1,8 +1,8 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
-module.exports = defineConfig({
+export default defineConfig({
   admin: {
     disable: process.env.NODE_ENV === 'production',
   },
