3063b49471c76b77f55992cc9429df040012b56a
Yash-Sheliya-Apexture <yash@apexture.in>
2025-11-19 11:44:41 +0530

    chore: add GCP VM deployment guide

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -1,32 +1,20 @@
-// import { loadEnv, defineConfig } from "@medusajs/framework/utils"
-
-// loadEnv(process.env.NODE_ENV || "development", process.cwd())
-
-// const config = defineConfig({
-//   admin: {
-//     disable: process.env.MEDUSA_ADMIN_DISABLED === 'true',
-//   },
-//   projectConfig: {
-//     databaseUrl: process.env.DATABASE_URL,
-//     redisUrl: process.env.REDIS_URL,
-//     workerMode: (process.env.MEDUSA_WORKER_MODE as "worker" | "shared" | "server" | undefined) || "worker",
-//     http: {
-//       storeCors: process.env.STORE_CORS!,
-//       adminCors: process.env.ADMIN_CORS!,
-//       authCors: process.env.AUTH_CORS!,
-//       jwtSecret: process.env.JWT_SECRET || "supersecret",
-//       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-//     }
-//   }
-// })
-
-// export default config
-// module.exports = config
-
-
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+const workerMode =
+  (process.env.MEDUSA_WORKER_MODE as 'worker' | 'shared' | 'server' | undefined) || 'shared'
+
+const databaseDriverOptions =
+  process.env.DATABASE_SSL === 'true'
+    ? {
+        connection: {
+          ssl: {
+            rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true',
+          },
+        },
+      }
+    : {}
 
 module.exports = defineConfig({
   admin: {
@@ -34,13 +22,15 @@ module.exports = defineConfig({
   },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions,
     redisUrl: process.env.REDIS_URL,
+    workerMode,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+  },
 })
